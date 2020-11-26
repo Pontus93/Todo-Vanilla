f7eaf112bf8dd39f9fc3fa56310b33bf1b682db0
Pontus <pontuspersson@outlook.com>
2020-11-26 15:33:17 +0100

    v1
    version 1, todolist vanillaJS

---
+++ app.js
@@ -0,0 +1,53 @@
+// QuerySelectors & focus on text input
+const addTodoInput = document.querySelector(".addTodoInputField");
+const todoList = document.querySelector(".toDoList");
+addTodoInput.focus();
+
+// Check if not empty then start our next function with the value of input
+addTodoForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (addTodoInput.value.length > 0) {
+        addTodo(addTodoInput.value);
+    }
+});
+
+// Add todo and reset for new text input
+function addTodo(todoText) {
+    todoList.insertAdjacentHTML(
+        "beforeend",
+        `<li class="liItem" onclick="completeTodo(this)">${todoText}
+        <icon class="icon" onclick="deleteTodo(this)">
+        <i class='fa fa-trash'></i></icon></li>`
+    );
+    addTodoForm.reset();
+    store();
+}
+
+// Delete function & set focus on text input
+function deleteTodo(element) {
+    element.parentElement.remove();
+    addTodoInput.focus();
+    store();
+}
+
+// when list-item is completed
+function completeTodo(element) {
+    element.classList.toggle("line");
+    addTodoInput.focus();
+}
+
+// localstorage for todos
+function store() {
+    window.localStorage.liItem = todoList.innerHTML;
+}
+
+function getValues() {
+    let storedValues = window.localStorage.liItem;
+    if (!storedValues) {
+    }
+    else {
+        todoList.innerHTML = storedValues;
+    }
+}
+getValues();
+
